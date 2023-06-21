31d60336eea02a233576adf03fad94aed54ac752
junsang-ee <dkwnstkddk@gmail.com>
2023-06-21 11:49:46 +0900

    Create BoardActivationStatus enum Add Board member (status)

---
+++ src/main/java/com/springboot/web/constants/BoardActivationStatus.java
@@ -0,0 +1,5 @@
+package com.springboot.web.constants;
+
+public enum BoardActivationStatus {
+    NORMAL, ERASED
+}
--- src/main/java/com/springboot/web/model/entity/BoardEntity.java
+++ src/main/java/com/springboot/web/model/entity/BoardEntity.java
@@ -1,5 +1,6 @@
 package com.springboot.web.model.entity;
 
+import com.springboot.web.constants.BoardActivationStatus;
 import com.springboot.web.constants.DisclosureStatusType;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,12 +20,21 @@ public class BoardEntity extends AbstractBaseEntity {
     @JoinColumn(name = "created_by")
     private UserEntity createdBy;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private BoardActivationStatus status;
+
     @Builder
     public BoardEntity(String name, DisclosureStatusType disclosureStatusType, UserEntity user) {
         this.name = name;
         this.createdBy = user;
         if (disclosureStatusType != null)
             this.setDisclosureStatusType(disclosureStatusType);
+    }
+
+    @PrePersist
+    public void onPrevisionPersist() {
+        this.status = BoardActivationStatus.NORMAL;
     }
 
 }
