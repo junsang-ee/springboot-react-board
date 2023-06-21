c54f01650922071198433622a617b84d63e3b360
devcheol <a7557z@gmail.com>
2023-06-21 16:18:47 +0900

    test: Repository Test

---
+++ src/test/java/com/springboot/web/MainApplicationTests.java
@@ -0,0 +1,10 @@
+package com.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainApplicationTests {
+    @Test
+    void contextLoads() {}
+}
