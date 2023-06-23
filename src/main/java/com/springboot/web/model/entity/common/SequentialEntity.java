964c91de95d4e234f3c454a7fb47e1440ff58805
junsang-ee <dkwnstkddk@gmail.com>
2023-06-23 14:53:24 +0900

    Create long type id value abstract entity and timestamp entity

---
+++ src/main/java/com/springboot/web/model/entity/common/SequentialEntity.java
@@ -0,0 +1,31 @@
+package com.springboot.web.model.entity.common;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@ToString
+@MappedSuperclass
+public abstract class SequentialEntity implements Persistable<Long> {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @Override
+    @JsonIgnore
+    public boolean isNew() {
+        return id == null;
+    }
+
+}
