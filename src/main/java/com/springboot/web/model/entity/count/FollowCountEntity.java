5a80e5b44bd2d9e559772f5ca5109ceba76133d2
junsang-ee <dkwnstkddk@gmail.com>
2023-06-27 14:51:44 +0900

    Modify persistence relationship ( UserEntity <---> FollowCountEntity) Add set user In followCountEntity logic

--- src/main/java/com/springboot/web/model/entity/count/FollowCountEntity.java
+++ src/main/java/com/springboot/web/model/entity/count/FollowCountEntity.java
@@ -1,15 +1,22 @@
 package com.springboot.web.model.entity.count;
 
+import com.springboot.web.model.entity.UserEntity;
 import com.springboot.web.model.entity.common.TimestampSequentialEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Entity(name = "follow_count")
 public class FollowCountEntity extends TimestampSequentialEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
 
     private long followerCount;
 
