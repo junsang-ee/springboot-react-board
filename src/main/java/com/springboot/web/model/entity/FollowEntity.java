938092ca716bf5257f73b7b3736c3a2e0876f30d
junsang-ee <dkwnstkddk@gmail.com>
2023-07-09 12:12:02 +0900

    Revert "Remove setter dependancy In FollowEntity"
    This reverts commit a1b44b437cc056606640d133d3155d42cb456f25.

--- src/main/java/com/springboot/web/model/entity/FollowEntity.java
+++ src/main/java/com/springboot/web/model/entity/FollowEntity.java
@@ -4,6 +4,7 @@ import com.springboot.web.model.entity.common.TimestampSequentialEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
