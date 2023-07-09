a1b44b437cc056606640d133d3155d42cb456f25
junsang-ee <dkwnstkddk@gmail.com>
2023-07-09 12:11:35 +0900

    Remove setter dependancy In FollowEntity Created ReplyEntity

--- src/main/java/com/springboot/web/model/entity/FollowEntity.java
+++ src/main/java/com/springboot/web/model/entity/FollowEntity.java
@@ -4,7 +4,6 @@ import com.springboot.web.model.entity.common.TimestampSequentialEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
